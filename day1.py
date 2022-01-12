a8c8b2f8ee51d42c719f8de10ce718717bf8de55
Zodiark619 <seonghyejin33@gmail.com>
2022-01-12 19:25:24 +0700

    first day

---
+++ day1.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print('Day 1 100daypython project')
+#2. Ask the user for the city that they grew up in.
+input1=input('What city were you spawn from?\n')
+#3. Ask the user for the name of a pet.
+input2=input('Name one of your pets\n')
+#4. Combine the name of their city and pet and show them their band name.
+result=input1+' '+input2
+print('Band name: '+result)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
